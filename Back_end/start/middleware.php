be6f2c2cfe8491ef43d899e64858f979df4a5cdd
imadovetch <ibouderoua63@gmail.com>
2024-01-10 12:56:25 +0100

    finaliser structure de backend

--- Back_end/start/middleware.php
+++ Back_end/start/middleware.php
@@ -3,6 +3,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 require_once "config/config.php";
+require_once "helpers/filter.php";
 class Heart {
     public $Method;
     public $url;
--- Back_end/start/models/Modelusers.php
+++ Back_end/start/models/Modelusers.php
@@ -1,3 +1,3 @@
 <?php
 include "../backwork/Dataopp.php";
-$users = DatabaseSingleton::getInstance('alpha', 'localhost', 'root', '');
+$users = DatabaseSingleton::getInstance(DB_NAME, DB_HOST, DB_USER, DB_PASSWORD);
