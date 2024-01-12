c1bcbd8e3c78e05133597c4d862789cc6ea3345b
imadovetch <ibouderoua63@gmail.com>
2024-01-12 14:34:16 +0100

    push 2.34

--- Back_end/start/config/config.php
+++ Back_end/start/config/config.php
@@ -1,6 +1,6 @@
 <?php
 define('URL_ROOT', 'http://localhost/Alpha');
-define('DB_NAME', 'alpha');
+define('DB_NAME', 'wikibase');
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
--- Back_end/start/controllers/GET/ControllerCategorie.php
+++ Back_end/start/controllers/GET/ControllerCategorie.php
@@ -1,14 +1,14 @@
 <?php
-echo json_encode('hi');
+require_once 'models/Modelcategorie.php';
+
 // echo "<br>". "controoooooooooolerpassed" . "<br>";
 if(isset($this->autorisation) && $this->autorisation === 1){
     
   
     http_response_code(200);
-    echo json_encode($Anonce->selectAll("categories"));
+    echo json_encode($categories->selectAll("categories"));
 
 
 }else{
-
   echo "no autorisation";  
 }
