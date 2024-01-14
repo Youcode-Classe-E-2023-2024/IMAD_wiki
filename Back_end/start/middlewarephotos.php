0585885600f087b8b101870d6837fe5aab2f463f
imadovetch <ibouderoua63@gmail.com>
2024-01-14 20:28:32 +0100

    archive

--- Back_end/start/middlewarephotos.php
+++ Back_end/start/middlewarephotos.php
@@ -5,6 +5,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     
     if (move_uploaded_file($_FILES['file']['tmp_name'], $targetFile)) {
         echo basename($_FILES['file']['name']);
+        // http://localhost/IMAD_wiki/Back_end/start/storage/Capture%20d'%C3%A9cran%202023-12-28%20142050.png
+       
         echo 'File uploaded successfully.';
     } else {
         echo 'Error uploading file.';
